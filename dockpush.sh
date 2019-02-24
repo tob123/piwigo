1b24ce2faee3c380ba1b180dc122697a996a0d57
tob <tob@nice.eu>
2019-02-24 22:55:15 +0100

    add latest and additional versioning

--- dockpush.sh
+++ dockpush.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 set -ex
 docker push tob123/piwigo-staging:${VERSION}
+if [[ -n $LATEST_MINOR ]]; then
+  MAJOR_TAG=$(echo $VERSION | awk -F. {' print $1"."$2'})
+  docker tag tob123/piwigo-staging:${VERSION} tob123/piwigo-staging:${MAJOR_TAG}
+  docker push tob123/piwigo-staging:${MAJOR_TAG}
+fi
 if [[ -n $LATEST ]]; then
   docker tag tob123/piwigo-staging:${VERSION} tob123/piwigo-staging:latest
   docker push tob123/piwigo-staging:latest
